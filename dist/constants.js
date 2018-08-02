90319e50189270314a7874130d4f316c27395407
Thomas Rollins <Thomas.Rollins@hotmail.ca>
2018-08-01 21:36:12 -0400

    removed /dist

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_PROVIDER = 'DbConnectionToken';
-exports.BOOK_MODEL_PROVIDER = 'BookModelToken';
-//# sourceMappingURL=constants.js.map
--- dist/schema.validation.pipe.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=schema.validation.pipe.js.map
