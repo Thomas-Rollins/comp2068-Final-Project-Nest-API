f43192a3121cff5111d5f05c2f8b063d4806f663
Thomas Rollins <Thomas.Rollins@hotmail.ca>
2018-08-10 23:08:34 -0400

    corrected IUser interface name

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,16 @@
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { User } from './interfaces/user.interface';
+import { IUser } from './interfaces/user.interface';
 import { UserDto } from './dto/user.dto';
-
 
 @Injectable()
 export class UsersService {
   constructor(
-    @Inject('UserModelToken') private readonly userModel: Model<User>) {
+    @Inject('UserModelToken') private readonly userModel: Model<IUser>) {
   }
 
   // GET: FindAll
-  public async findAll(): Promise<User[]> {
+  public async findAll(): Promise<IUser[]> {
     return await this.userModel.find().exec();
   }
 
@@ -25,13 +24,13 @@ export class UsersService {
   }
 
   // POST: Create
-  async create(userDto: UserDto): Promise<User> {
+  async create(userDto: UserDto): Promise<IUser> {
     const createdUser = new this.userModel(userDto);
     return await createdUser.save();
   }
 
   // PUT: Update
-  async update(id: string, userDto: UserDto): Promise<User> {
+  async update(id: string, userDto: UserDto): Promise<IUser> {
     await this.userModel.update({ _id: id }, userDto, { upsert: false });
     const user = await this.userModel.findById(id);
     if (user == null) {
