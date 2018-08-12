5d8e48596ba02004a2561c48e85c433315b01b7c
Thomas Rollins <Thomas.Rollins@hotmail.ca>
2018-08-11 20:36:13 -0400

    Disabled auth on all routes except user.DELETE

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   root(): string {
-    return 'Hello World!';
+    return '...this is an API go away.';
   }
 }
